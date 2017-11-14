4bc6eebeff6330ca9caf63d2a9c61bdea286b509
César Freire <cfreire@cfreire.com.pt>
2017-11-14 10:17:09 +0000

    versão factory

--- 09_HelloWorldFactory/src/main/java/com/redoutevant/helloworldfactory/Main.java
+++ 09_HelloWorldFactory/src/main/java/com/redoutevant/helloworldfactory/Main.java
@@ -1,22 +1,35 @@
 package com.redoutevant.helloworldfactory;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Factory
+ *
  * @author Cesar Freire
  */
 public class Main {
-    
+
 // versão 1
 //    public static void main(String[] args) {
 //        System.out.println("Hello World!");
 //    }
-    
 // versão 2
 //    public static void main(String[] args) {
 //       if(args.length > 0) {
 //           System.out.println(args[0]);
 //       }
 //    }
-    
- // versão 3   
+    // versão 3   
+//    public static void main(String[] args) {
+//        Display d = new DisplayImpl();
+//        Mensageiro m = new MensageiroImpl();
+//        d.setMensagem(m);
+//        try {
+//            d.render();
+//        } catch (Exception ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+    // versao 4
 }
